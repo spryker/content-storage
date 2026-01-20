8e1aeb73a869e2ba25abc1533578a96da7460428
Serhii Chepela <serhii.chepela@spryker.com>
2026-01-20 16:32:33 +0200

    FRW-11265 Introduced Symfomy Messenger as a replacement for the RabbitMq transport (#172)
    FRW-11265 Integration of Symfony/Messenger to Spryker

--- tests/SprykerTest/Zed/ContentStorage/Business/ContentStorageFacadeTest.php
+++ tests/SprykerTest/Zed/ContentStorage/Business/ContentStorageFacadeTest.php
@@ -102,6 +102,7 @@ class ContentStorageFacadeTest extends Unit
         $this->tester->setDependency(QueueDependencyProvider::QUEUE_ADAPTERS, function (Container $container) {
             return [
                 $container->getLocator()->rabbitMq()->client()->createQueueAdapter(),
+                $container->getLocator()->symfonyMessenger()->client()->createQueueAdapter(),
             ];
         });
 
